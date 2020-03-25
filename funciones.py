734fe89410b9e789806032241bf7d2ec2b4cfd16
Fernando Palomera <fernando@MacBook-Pro-de-Fernando.local>
2020-03-25 00:21:48 -0300

    modified the models implementations

--- funciones.py
+++ funciones.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from time import time
-from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder, LabelEncoder
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import make_column_transformer
 from sklearn.model_selection import train_test_split
@@ -37,7 +37,6 @@ def distribution_plots(df, columns=3):
                 ax.text(p.get_x()+p.get_width()/2., height + .5,
                     '{:1.2f}'.format(height/len(df[i])), ha="center")
 
-            
         if df[i].dtype == 'float64':
             fig.add_subplot(rows, columns, n+1)
             ax = sns.distplot(df[i])
@@ -50,28 +49,16 @@ def distribution_plots(df, columns=3):
 
 # Función de preprocesamiento de base
 
-def pre_processing(df, num_cols, obj_cols, exclude, outliers_cols, target, 
-                    drop_nan=False, remove_outliers=False, std_scaler=False, 
+def pre_processing(df, num_cols, obj_cols, exclude, target, std_scaler=False, 
                     one_hot=True, custom_split=True):
     """
     Función para aplicar un preprocesamiento de datos sobre un dataframe
 
     """
 
+    #columns = [target]+num_cols+obj_cols+exclude
     columns = num_cols+obj_cols+exclude
     tmp = df[columns]
-    
-    # Remoción de filas con datos perdidos
-    if drop_nan:
-        tmp = tmp.dropna()
-
-   # Detección y eliminación de outliers
-    if remove_outliers:
-     
-        Q1 = tmp[outliers_cols].quantile(0.25)
-        Q3 = tmp[outliers_cols].quantile(0.75)
-        IQR = Q3 - Q1
-        tmp = tmp[~((tmp < (Q1 - 1.5 * IQR)) |(tmp > (Q3 + 1.5 * IQR))).any(axis=1)]
     
     num_steps, obj_steps = None, None
     
@@ -87,6 +74,7 @@ def pre_processing(df, num_cols, obj_cols, exclude, outliers_cols, target,
         tuples = [(var, list(tmp2[var].value_counts().sort_values(ascending=True).index)[1:]) for var in tmp2]
         dummy_names = ['{}_{}'.format(tup[0], cat) for tup in tuples for cat in tup[1]]
         # Renombre de columnas df final
+        #columns = [target]+num_cols+dummy_names+exclude
         columns = num_cols+dummy_names+exclude
         target = ['{}_{}'.format(tup[0], tup[1][0]) for tup in tuples if tup[0] == target][0]
 
@@ -95,6 +83,7 @@ def pre_processing(df, num_cols, obj_cols, exclude, outliers_cols, target,
     obj_pipe = make_pipeline(obj_steps)
 
     column_transformer = make_column_transformer(
+                        #(OrdinalEncoder(), target),
                             (num_pipe, num_cols),
                             (obj_pipe, obj_cols),
                             ('passthrough', exclude))
@@ -124,16 +113,6 @@ def pre_processing(df, num_cols, obj_cols, exclude, outliers_cols, target,
     to_delete = [var for var in X_train if len(X_train[var].unique()) == 1]
     X_train = X_train.drop(columns=to_delete)
     X_test = X_test.drop(columns=to_delete)
-
-    # to_delet2 
-    #for x in to_delete+[target]: 
-    #    dummy_names.remove(x)
-    
-    #to_delete2 = [var for var in dummy_names
-    #                if X_train[var].value_counts('%').sort_values()[0] < .01]
-    #X_train = X_train.drop(columns=to_delete2)
-    #X_test = X_test.drop(columns=to_delete2)
-    #print(to_delete2)
 
     # Aegurar tipo de dato para entrenar y modelos modelos
     X_train = X_train.astype('int')
